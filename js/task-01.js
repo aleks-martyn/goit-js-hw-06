d973b2817db2654f388a6ebaaad0126787c3a069
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-18 18:50:02 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,28 +1,17 @@
-const listWithId = document.querySelector('#categories');
-console.log(listWithId);
-
-
 const itemsByClass = document.querySelectorAll('.item');
-console.log(itemsByClass);
 
 const numberOfCategories = itemsByClass.length;
 console.log('Number of categories:', numberOfCategories);
 
-const itemElements = itemsByClass[0].children;
-console.log(itemElements);
+itemsByClass.forEach(item => {
+
+const itemElements = item.children;
 
 const titleOfElement = itemElements[0].textContent;
 console.log('Category:', titleOfElement);
 
 const listOfElement = itemElements[1].children;
-console.log(listOfElement);
 
 const lengthOfListOfElement = listOfElement.length;
 console.log('Elements:',lengthOfListOfElement);
-
-
-
-const allTitleEl = document.querySelectorAll('h2');
- allTitleEl.forEach(el => {
-  console.log('Category:', el.textContent);
 });
