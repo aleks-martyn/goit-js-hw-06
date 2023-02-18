979c7bf53245fea4fe6b9950a5b2c07a31d967c3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-19 00:04:39 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+
+  return listItem;
+});
+
+console.log(elements);
+
+listEl.append(...elements);
+
