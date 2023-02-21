e61f4d68d0e61529301eb92dd2f6425e845c7b23
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-21 17:31:26 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector('#validation-input');
+console.log(inputEl.dataset.length);
+
+inputEl.addEventListener('blur', toCheckQuantityOfNumbers);
+
+function toCheckQuantityOfNumbers(event) {
+  
+}
