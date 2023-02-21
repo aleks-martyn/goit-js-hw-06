74b7c174154030641ba79c2f85d0a865afdbc13e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-21 18:32:13 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,13 @@
 const inputEl = document.querySelector('#validation-input');
-console.log(inputEl.dataset.length);
 
 inputEl.addEventListener('blur', toCheckQuantityOfNumbers);
 
 function toCheckQuantityOfNumbers(event) {
-  
+  console.log(event.currentTarget.value.length);
+  if(event.currentTarget.value.length !== Number(inputEl.dataset.length)) {
+    inputEl.classList.add('invalid');
+  } else if(event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+  inputEl.classList.remove('invalid');
+  inputEl.classList.add('valid');
+}
 }
