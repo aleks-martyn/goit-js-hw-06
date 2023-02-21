71cba8be3df89b4b9b24921ce029386a8c62c01a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-22 00:51:15 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.dir(event.currentTarget.elements);
+
+    const formElements = event.currentTarget.elements;
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if(mail === '' || password === '') {
+        return alert('Всі поля мають бути заповнені!');
+    }
+}
