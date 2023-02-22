603ca8fc9f61272abf7de1aa5f1eb8b681192ed8
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-23 01:01:01 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector('body');
+
+const spanEl = document.querySelector('.color');
+
+const buttonEl = document.querySelector('.change-color');
+
+buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = spanEl.textContent;
+}
