877ec7b5b9c9e4777397188698131b71625b98f2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-23 20:20:48 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,12 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  if (event.currentTarget.value.length === Number(inputEl.dataset.length)) {
+  if (event.currentTarget.value.length === 0) {
+    inputEl.classList.remove('invalid');
+    inputEl.classList.remove('valid');
+  } else if (
+    event.currentTarget.value.length === Number(inputEl.dataset.length)
+  ) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
   } else {
