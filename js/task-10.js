c90a71432e2b25de3e2e2daa8cf7d155cfab673b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-23 16:58:20 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,4 +3,26 @@ function getRandomHexColor() {
 }
 
 const inputEl = document.querySelector('input');
-console.log(inputEl)
+console.log(inputEl);
+
+const createBtnEl = document.querySelector('button[data-create]');
+console.log(createBtnEl);
+
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+console.log(destroyBtnEl);
+
+const boxesEl = document.querySelector('#boxes');
+console.log(boxesEl);
+
+createBtnEl.addEventListener('click', getAmount);
+
+function getAmount(event) {
+  return createBoxes(inputEl.value);
+}
+
+function createBoxes(amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    const box = document.createElement('div');
+    console.log(box);
+  }
+}
