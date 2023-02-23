622c172a959c5a62ddca162c5311161b0d843bf4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-02-23 18:57:00 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -12,13 +12,29 @@ const boxesEl = document.querySelector('#boxes');
 
 createBtnEl.addEventListener('click', getAmount);
 
-function getAmount(event) {
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+function getAmount() {
   return createBoxes(inputEl.value);
 }
 
 function createBoxes(amount) {
+  const arrayBoxes = [];
+  let width = 20;
+  let height = 20;
   for (let i = 1; i <= amount; i += 1) {
     const box = document.createElement('div');
+    box.style.cssText = `width: ${(width += 10)}px; height: ${(height += 10)}px; background-color: ${getRandomHexColor()};`;
+
     console.log(box);
+    arrayBoxes.push(box);
+    console.log(arrayBoxes);
   }
+  boxesEl.append(...arrayBoxes);
+  console.log(boxesEl);
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  console.log(boxesEl.innerHTML);
 }
